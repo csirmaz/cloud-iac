fbbbf5532c01a19734d12c9acccea3c7526e4abc
csirmaz <code@epcsirmaz.co.uk>
2025-10-05 23:14:25 +0100

    incremental model

---
+++ dbt/dbt_sample/models/elod1/silver1.sql
@@ -0,0 +1,24 @@
+
+{{ config(materialized='incremental') }}
+
+
+/*
+- Incremental materialization
+- Reference a source defined in the yml file
+- Update the source by running the add_data.py script
+*/
+
+
+select
+    unixtime,
+    personid,
+    sum(price)
+from {{ source('bsg','bronze_data') }}
+{% if is_incremental() %}
+    -- https://docs.getdbt.com/docs/build/incremental-models
+    -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+    where unixtime > (select coalesce(max(unixtime), 0) from {{this}})
+{% endif %}
+group by unixtime, personid
+
+
